6c9f1a80fc131e8f408537921ed6d5453bc4752b
Stepan Grankin <sgrankin@inbox.ru>
2021-03-08 22:14:33 +0300

    AppInfo: replace JsonObject attribute with naming strategy to JsonProperty with strong property name

--- src/Sber.SmartApp.Models/AppInfo.cs
+++ src/Sber.SmartApp.Models/AppInfo.cs
@@ -1,18 +1,29 @@
 ﻿using System;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
 
 namespace Sber.SmartApp.Models
 {
-    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
     public class AppInfo
     {
+        [JsonProperty(PropertyName = "projectId")]
         public Guid ProjectId { get; set; }
 
+        [JsonProperty(PropertyName = "applicationId")]
         public Guid? ApplicationId { get; set; }
 
+        [JsonProperty(PropertyName = "appversionId")]
         public Guid? AppversionId { get; set; }
 
+        [JsonProperty(PropertyName = "frontendType")]
         public string FrontendType { get; set; }
+
+        [JsonProperty(PropertyName = "frontendEndpoint")]
+        public string FrontendEndpoint { get; set; }
+
+        [JsonProperty(PropertyName = "systemName")]
+        public string SystemName { get; set; }
+
+        [JsonProperty(PropertyName = "frontendStateId")]
+        public string FrontendStateId { get; set; }
     }
 }
